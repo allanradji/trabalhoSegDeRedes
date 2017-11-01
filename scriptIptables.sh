1c0e911119e425291caed2c7282f89473a166098
all_radji <allanradji@gmail.com>
2017-11-01 00:37:57 -0300

    commit

---
+++ scriptIptables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Questão 1
+
+regras=iptables -L
+if [ $regras != '' ]; then
+        iptables -L
+else
+	iptables -P INPUT ACCEPT
+	iptables -P OUTPUT ACCEPT
+	iptables -P FORWARD ACCEPT
+fi
